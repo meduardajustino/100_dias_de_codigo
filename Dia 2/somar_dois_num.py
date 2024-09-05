5d573dedb53880b9008279ac414a124546f478c4
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-09-05 15:00:00 -0300

    Dia 2

---
+++ Dia 2/somar_dois_num.py
@@ -0,0 +1,6 @@
+print ("Vamos somar dois números ⬇️ 📐")
+num_1 = float(input("Digite o primeiro número:"))
+num_2 = float(input("Digite o segundo número:"))
+
+soma_final = num_1 + num_2
+print(f'A soma dos dois números é: {soma_final}')
