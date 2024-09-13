fa17b5b98679cdef1265d2f0a7791f8aa7972069
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-09-12 23:33:21 -0300

    Dia 9

--- Dia 9/dados_dia_9.py
+++ Dia 9/dados_dia_9.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 
-import pandas as pd
-
 # considerando os dados da receita de uma loja de cosméticos
 dados_logistica = {
     'Produto': ['Chapinha', 'Escova secadora', 'Hidratante facial', 'Base Cosmos', 'Sérum', 'Perfume Floratta'],
@@ -15,3 +13,17 @@ df = pd.DataFrame(dados_logistica)
 df['Vendas'] = df['Quantidade'] * df['Preco']
 
 print(df)
+
+# Estatística descritiva da receita (df.describe())
+media = df['Vendas'].mean()
+mediana = df['Vendas'].median()
+desvio_padrao = df['Vendas'].std()
+maximo = df['Vendas'].max()
+minimo = df['Vendas'].min()
+
+print('˚˖𓍢ִ໋🌷͙֒✧˚.🎀༘⋆  Os dados estatísticos semanais da empresa estão disponíveis abaixo  ˚˖𓍢ִ໋🌷͙֒✧˚.🎀༘⋆')
+print(f'🩰 A receita média foi {media:.3f};')
+print(f'🪞  A mediana é {mediana:.0f};')
+print(f'🦢 O desvio padrão é de {desvio_padrao:.3f};')
+print(f'🫧  A receita máximo foi de {maximo};')
+print(f'🍯 A receita mínima foi de {minimo}.')
