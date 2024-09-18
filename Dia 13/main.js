d4e176456d0b89a0b7347d22c9ef98affa014f9e
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-09-17 23:16:39 -0300

    update flip clock - dia 13

--- Dia 13/main.js
+++ Dia 13/main.js
@@ -1,11 +1,11 @@
 function atualizarRelogio() {
     const clockElement = document.getElementById('clock');
     const agora = new Date();
-    
+
     let horas = String(agora.getHours()).padStart(2, '0');
     let minutos = String(agora.getMinutes()).padStart(2, '0');
     let segundos = String(agora.getSeconds()).padStart(2, '0');
-    
+
     clockElement.innerHTML = `${horas}:${minutos}:${segundos}`;
 }
 
