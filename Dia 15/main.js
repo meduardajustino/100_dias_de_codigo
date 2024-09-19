43e8b60e9bf5554acebe41279d243ff722063a96
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-09-18 22:27:24 -0300

    dia 15 - contador de caracteres

---
+++ Dia 15/main.js
@@ -0,0 +1,10 @@
+const textarea = document.querySelector('textarea');
+const count = document.querySelector('.count');
+
+function contarLetras()
+{
+    const texto = textarea.value;
+    const textoLength = textarea.value.length;
+
+    count.innerText = `${textoLength}`;
+}
