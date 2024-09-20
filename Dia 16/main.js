7f253e626cb07dbb0f2fb145f67f5cd3576190cb
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-09-20 10:58:49 -0300

    Create main.js

---
+++ Dia 16/main.js
@@ -0,0 +1,37 @@
+const slides = document.querySelectorAll('#imagens-carrossel img');
+const botoes = document.querySelectorAll('.btn-carrossel');
+let slideAtual = 0;
+let intervaloCarrossel;
+
+// trocar o slide ativo
+const trocarSlide = (indice) => {
+    slides.forEach(slide => slide.classList.remove('ativo'));
+    botoes.forEach(botao => botao.classList.remove('ativo'));
+
+    slides[indice].classList.add('ativo'); // slide atual
+    botoes[indice].classList.add('ativo'); // botão correspondente 
+  
+    document.getElementById('imagens-carrossel').style.transform = translateX(-${indice * 600}px);
+}
+
+const repetir = () => {
+    intervaloCarrossel = setTimeout(() => {
+        slideAtual++;
+        if (slideAtual >= slides.length) {
+            slideAtual = 0;
+        }
+        trocarSlide(slideAtual);
+        repetir();
+    }, 2000); // coloquei intervalo de 2 segundos
+}
+
+repetir();
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        clearTimeout(intervaloCarrossel);
+        slideAtual = indice;
+        trocarSlide(indice);
+        repetir(); // reinicia o carrossel
+    });
+});
