eb166278c1fa21fadf8cfa4bc6a67b06da3c87a6
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-10-06 00:21:09 -0300

    add componentes em React

---
+++ Dia 32/Dia32_codi/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import MyComponent from './components/MyComponent'
+
+function App() {
+
+  return (
+    <div>
+      <MyComponent/>
+    </div>
+  )
+}
+
+export default App;
---
+++ Dia 32/Dia32_codi/src/components/MyComponent.jsx
@@ -0,0 +1,10 @@
+function MyComponent(){
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <p>Este é o meu primeiro componente</p>
+        </div>
+    )
+}
+
+export default MyComponent;
