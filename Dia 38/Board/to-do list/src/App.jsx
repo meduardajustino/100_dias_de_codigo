948140ecc630ed12035d956466cf5f8b070a459b
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-10-11 22:27:05 -0300

    Quadro de tarefas com react e tailwind

---
+++ Dia 38/Board/to-do list/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Input from './components/input'
+function App() {
+  return (
+      <>
+      <h1>TODO LIST @iemstudies</h1>
+        <Input/>
+      </>
+  )
+}
+
+export default App;
