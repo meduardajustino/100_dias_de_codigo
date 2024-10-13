b262840f6ce13c379effe77a9e5ebd29782dffa6
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-10-13 12:59:38 -0300

    Melhoris no todo list

--- Dia 38/Board/to-do list/src/App.jsx
+++ Dia 38/Board/to-do list/src/App.jsx
@@ -2,23 +2,22 @@ import {React, useState} from "react";
 import Input from './components/input';
 
 function App() {
+  
   const [taskList, setTaskList] = useState([]);
 
-  console.log(taskList);
   return (
-    <div className="flex flex-col items-center justify-center py-8 gap-4">
-      <h1>TODO LIST @iemstudies</h1>
-        <Input taskList={taskList} setTaskList={setTaskList}/>
+    <div className="flex flex-col items-center justify-center py-8 gap-4 bg-[url('https://i.ibb.co/3TJQ4V1/wallpaper.jpg')] bg-cover bg-center min-h-screen">
+      <h1 className="text-4xl font-bold text-white">TODO LIST @iemstudies</h1>
+      
+      <Input taskList={taskList} setTaskList={setTaskList} />
 
-        <div>
-          {taskList.map((task, index) => (
-            <li key={index}>{task}</li>
-          ))}
-        </div>
-
-
+      <div className="text-white">
+        {taskList.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
       </div>
-  )
+    </div>
+  );
 }
 
 export default App;
--- Dia 38/Board/to-do list/src/components/input.jsx
+++ Dia 38/Board/to-do list/src/components/input.jsx
@@ -1,31 +1,36 @@
-import React from "react"
-import { useState } from "react"
+import React from "react";
+import { useState } from "react";
 
-const Input = (taskList, setTaskList) => {
+const Input = ({ taskList, setTaskList }) => {
     const [input, setInput] = useState("");
-    
-    const hadleAddTask = (e) => {
+
+    const handleAddTask = (e) => {
         e.preventDefault();
         setTaskList([...taskList, input]);
-        setInput=("");
-    }
+        setInput("");
+    };
 
     console.log(input);
-    
+
     return (
         <>
-            <form className = "flex flex-row items-center gap-3">
-                <input type="text"
+            <form className="flex flex-row items-center gap-3">
+                <input 
+                    type="text"
                     className="border rounded-lg py-1.5 px-2.5"
                     placeholder="Adicione uma tarefa"
                     value={input}
-                    onChange={(e) => setInput(e.target.value)}/>
-                <button onClick={hadleAddTask}
-                        className="bg-pink-400 text-white py-2 px-3.5 rounded-lg
-                        font-semibold hover:opacity-70">Adicionar Tarefa</button>
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button 
+                    onClick={handleAddTask}
+                    className="bg-pink-400 text-white py-2 px-3.5 rounded-lg font-semibold hover:opacity-70"
+                >
+                    Adicionar Tarefa
+                </button>
             </form>
         </>
-    )
-}
+    );
+};
 
-export default Input
+export default Input;
