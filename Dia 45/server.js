ac1331ca6fcf12c1e1fb90c565d96470e26d4d49
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-10-18 22:19:01 -0300

    Conectando Frontend e Backend

---
+++ Dia 45/server.js
@@ -0,0 +1,31 @@
+//arquivo do servidor
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+
+// api criada no the cat api: live_Uj3YoB1ErU0LTxZ4agTRnarnsVD0m4uLHpI3zaeYHq4DFFGPLzgWIA9nI4psCX5e
+
+const api_key = 'live_Uj3YoB1ErU0LTxZ4agTRnarnsVD0m4uLHpI3zaeYHq4DFFGPLzgWIA9nI4psCX5e';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// criando rota para obter os dados da API
+app.get('/random-cats', async (req, res) => {
+    // consome a api com axios
+    const response = await axios.get('https://api.thecatapi.com/v1/imagens/search', {
+        headers : {
+            "x-api-key": api_key
+        },
+    });
+    // exibindo a resposta da requisição
+    res.json(response.data);
+});
+
+// criando o servidor
+const port = 2121;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta http://localhost:${port}`);
+})
