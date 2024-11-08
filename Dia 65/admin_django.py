4adf2939c764a008e230cee173ae2d08aaf8773b
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-11-08 20:30:04 -0300

    Administração no Django

---
+++ Dia 65/admin_django.py
@@ -0,0 +1,8 @@
+'''🗂️Dia 65 – 08/11/24
+Administração no Django
+
+Registre os modelos no arquivo admin.py do seu aplicativo Django para gerenciar
+dados através da interface administrativa do Django.
+
+Acesse o Django Admin e adicione alguns produtos ao banco de dados utilizando a
+interface de administração.'''
