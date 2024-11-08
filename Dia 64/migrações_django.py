f731c34d5bdfa8acd7c9010cb176ad643bd14eec
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-11-08 20:28:31 -0300

    Migrações no Django

--- Dia 64/migrações_django.py
+++ Dia 64/migrações_django.py
@@ -1,1 +1,7 @@
 # Para conferir o código do dia 64 - migrações no django veja o repositório do dia 61
+
+'''🗂️Dia 64 – 07/11/24
+Migrações no Django
+
+Execute python manage.py makemigrations e python manage.py migrate para
+criar e aplicar migrações no banco de dados.'''
