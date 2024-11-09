227f6e86b6910a3ce3c5775d255bfbf218604e97
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-11-09 18:37:50 -0300

    Views no Django

---
+++ Dia 66/views_django.py
@@ -0,0 +1,13 @@
+'''🗂️Dia 66 – 09/11/24
+Views no Django
+(para acessar as alterações veja a pasta do dia 61)
+
+
+Crie templates HTML para renderizar as páginas usando o mecanismo de template
+do Django.
+
+Desenvolva um template para a página que lista os produtos. Utilize tags do Django
+template language para exibir as informações dos produtos.
+
+Crie uma view que liste todos os produtos cadastrados. Esta view deve renderizar
+um template mostrando os detalhes de cada produto.'''
