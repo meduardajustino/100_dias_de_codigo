0b4bdde8a99b11e7d7f0cca8512027db7902bca3
Maria Eduarda Justino <madujustinostudies@gmail.com>
2024-11-11 23:14:49 -0300

    URLs no Django

--- Dia 61/meublog/blog/views.py
+++ Dia 61/meublog/blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 def home(request):
     posts = Post.objects.all()
     return render(request, 'blog/home.html', {'posts': posts})
 
 def post_detail(request, id):
-    post = get_object_or_400(Post, id=id)
+    post = get_object_or_404(Post, id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def new_post(request):
@@ -15,6 +16,7 @@ def new_post(request):
         if form.is_valid():
             form.save()
             return redirect('home')
-        else:
-            form = PostForm()
-            return render(request, 'blog/new_post.html', {'form': form})
+    else:
+        form = PostForm()
+    
+    return render(request, 'blog/new_post.html', {'form': form}) 
---
+++ Dia 68/urls_django.py
@@ -0,0 +1,10 @@
+'''Dia 68 – 11/11/24
+URLs no Django
+
+Confira a pasta 61 com atualizações
+
+Defina padrões de URL no arquivo urls.py para associar URLs a funções ou classes
+de views.
+
+Crie URLs para a lista de produtos e para uma página detalhada de cada produto.
+Configure o arquivo urls.py do aplicativo “core”.'''
